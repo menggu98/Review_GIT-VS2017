d5c55713e1cab0fb71677dd95fdfd8b7f5100816
smg <myengkyun@naver.com>
2026-01-19 13:49:50 +0900

    fix : 메세지 내용 수정

--- Review_GitTest/Review_GitTest/Form1.vb
+++ Review_GitTest/Review_GitTest/Form1.vb
@@ -15,6 +15,6 @@ Partial Public Class Form1
     End Sub
 
     Private Sub SimpleButton2_Click(sender As Object, e As EventArgs) Handles SimpleButton2.Click
-        MsgBox("GIT TEST2")
+        MsgBox("GIT TEST3")
     End Sub
 End Class
